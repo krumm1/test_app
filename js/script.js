a89521fa758e6208016026e3bfb06e78e033cbb4
Roma <frontend@pikpr.ru>
2020-04-27 17:00:15 +1100

    Добавил отступы

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ $('.app-slider').slick({
     arrows: false,
     dots: true,
     variableWidth: true,
-    swipeToSlide: true
+    swipeToSlide: true,
+    waitForAnimate: false
 });
 $('.app-cats').on('click', '.app-cats-item', function () {
     $(this).toggleClass('active');
-})
+});
